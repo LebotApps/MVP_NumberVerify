699052a822a87acf3558be74f8f231b400ce4cb6
Lebot Apps <lebot.llc@gmail.com>
2021-05-24 14:14:23 -0400

    updated HTML and JS file

--- js/script.js
+++ js/script.js
@@ -1,47 +1,32 @@
-// API 9d5acbb007c74165b9702ec4a7246e81
-
-// data structure https://phonevalidation.abstractapi.com/v1/?api_key=9d5acbb007c74165b9702ec4a7246e81&phone=14152007986
-
-// project requirements https://seir-everest.netlify.app/unit-projects/unit-one-project-requirements/
-
-
 let verifyNumber;
 
-const $number = $("#number");
+let $inputText;
+
+const $number = $(`#number`);
 const $carrier = $(`#carrier`);
 const $location = $(`#location`);
 
-function render(){
-
-}
-
-
-
 function handleGetData(event) {
     event.preventDefault();
-
-    searchText= $("input#main").val();
-
+    
+    inputText = $("#main").val();
 $.ajax({
     
-    url: `https://phonevalidation.abstractapi.com/v1/?api_key=9d5acbb007c74165b9702ec4a7246e81&phone=14707788215`
-
+    url: `https://phonevalidation.abstractapi.com/v1/?api_key=9d5acbb007c74165b9702ec4a7246e81&phone=${1 + inputText}`,
 })
-
     .then(
-
-
     function (data) {
+        
         verifyNumber = data;
 
-        $number.text(data["phone", "valid"]);
+        $number.text(data["valid"]);
         $carrier.text(data["carrier"]);
         $location.text(data["location"]);
+        
     },
-
 function (error) {
   console.log("bad request: ", error);
-})
-};
-
+}
+);
+}
 $("form").on('submit', handleGetData);
