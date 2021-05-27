75b2df8029c66a07aedf0cb364ee2482ee0b8a9b
Lebot Apps <lebot.llc@gmail.com>
2021-05-27 13:34:26 -0400

    updated readme and beautified code

--- README.md
+++ README.md
@@ -18,10 +18,24 @@ Mobile
 Desktop
 ![screenshot](imgs/DesktopWireFrame.jpg)
 
+# Getting Started 
+*click here to see deployed app*
+
+Enter your number in the field and press "Enter" or "Verify".
+
+![screenshot](imgs/GettingStarted1.jpg)
+
+Information on the number searched will return the following info
+
+![screenshot](imgs/GettingStarted2.jpg)
+
+
+
+# Future Enhancements
+- The ability to save searches 
+- Verify international numbers
+
 
 Thanks for enjoying Number Verify.
-
-
-
 
 Copyright © 2021 LebotApps 
--- js/script.js
+++ js/script.js
@@ -2,38 +2,48 @@
 //add screenshoots of actual app to screenshots
 //future enhancements (international calls and saved searches)
 
+// Declaring Variables 
 
 let verifyNumber;
 
 let $inputText;
 
+// jQuery
 const $number = $(`#number`);
 const $valid = $(`#valid`);
 const $carrier = $(`#carrier`);
 const $location = $(`#location`);
 
+// Functions 
+
 function handleGetData(event) {
     event.preventDefault();
-    
-    inputText = $("#main").val();
-$.ajax({
-    
-    url: `https://phonevalidation.abstractapi.com/v1/?api_key=9d5acbb007c74165b9702ec4a7246e81&phone=${1 + inputText}`,
-})
-    .then(
-    function (data) {
-        
-        verifyNumber = data;
 
-        $number.text(data["phone"]);
-        $valid.text(data["valid"]);
-        $carrier.text(data["carrier"]);
-        $location.text(data["location"]);
+
+//API 
+
+inputText = $("#main").val();
+    
+    $.ajax({
+
+        url: `https://phonevalidation.abstractapi.com/v1/?api_key=9d5acbb007c74165b9702ec4a7246e81&phone=${1 + inputText}`,
+        })
         
+//Promise
+
+    .then(function (data) {
+
+    verifyNumber = data;
+
+    $number.text(data["phone"]);
+    $valid.text(data["valid"]);
+    $carrier.text(data["carrier"]);
+    $location.text(data["location"]);
+
     },
-function (error) {
-  console.log("bad request: ", error);
-}
-);
+    function (error) {
+    console.log("bad request: ", error);
+    }
+    );
 }
 $("form").on('submit', handleGetData);
